a8f03f074039ae842babf6e6fb31dc23150da686
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2021-10-27 21:13:56 -0400

    Creating initial .py file.
    Created the initial file of a small mad libs game.

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+animal = input("Enter an animal: ")
+
+print("Violets are " + color + ".")
+print(plural_noun + " is green.")
+print("A " + animal + " is huge!")
